9da06b103757bf44f8a8c5a438019d4e4fa2f607
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-05-28 10:28:00 +0000

    Plugins model

--- lib/MyApp/Model/Plugins.pm
+++ lib/MyApp/Model/Plugins.pm
@@ -6,11 +6,34 @@ use experimental qw(signatures);
 
 use Mojo::Util qw(secure_compare);
 
-my $PLUGINS = {
-    abc      => 'abc',
-    def    => 'def'
-};
+use Mojo::Base -base;
 
-sub new ($class) { bless {}, $class }
+has 'sqlite';
+
+sub fetch {
+    my $self = shift;
+    return $self->sqlite->db->select( 'plugins', undef,
+        [ { has_new_screenshot => 1 }, { test_failure => { '!=' => undef } } ] )
+      ->hashes->to_array;
+}
+
+sub fetch_all {
+    my $self = shift;
+    return $self->sqlite->db->select('plugins')->hashes->to_array;
+}
+
+sub get {
+    my $self = shift;
+    my $name = shift;
+    return $self->sqlite->db->select( 'plugins', undef, { name => $name } )
+      ->hash;
+}
+
+sub update {
+    my $self = shift;
+    my $name = shift;
+    my $info = shift;
+    $self->sqlite->db->update( 'plugins', $info, { name => $name } );
+}
 
 1;
