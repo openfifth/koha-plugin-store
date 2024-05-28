cfcf0ee11cc7bb2da83e613cd707b90ceffa92fb
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-05-28 10:27:25 +0000

    Disable auth for now. Plugins routes

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -1,20 +1,35 @@
 package MyApp;
 use Mojo::Base 'Mojolicious', -signatures;
+use Mojo::SQLite;
 
 use MyApp::Model::Users;
+use MyApp::Model::Plugins;
 
 sub startup ($self) {
 
     $self->secrets( ['Mojolicious rocks'] );
     $self->helper( users => sub { state $users = MyApp::Model::Users->new } );
 
+    $self->helper(
+        sqlite => sub { state $sql = Mojo::SQLite->new('sqlite:database.db') }
+    );
+    $self->helper(
+        plugins => sub {
+            state $plugins = MyApp::Model::Plugins->new(
+                sqlite => shift->sqlite );
+        }
+    );
+
     my $r = $self->routes;
-    $r->any('/')->to('login#index')->name('index');
+    # $r->any('/')->to('login#index')->name('index');
 
-    my $logged_in = $r->under('/')->to('login#logged_in');
-    $logged_in->get('/protected')->to('login#protected');
+    # my $logged_in = $r->under('/')->to('login#logged_in');
+    # $logged_in->get('/protected')->to('login#protected');
 
-    $r->get('/logout')->to('login#logout');
+    # $r->get('/logout')->to('login#logout');
+
+    $r->get('/plugins')->to('plugins#list_all');
+
 }
 
 1;
