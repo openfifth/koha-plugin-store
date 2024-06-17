49b0ce4f8efbb6bf29beec21572c5c7ebee359ca
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-06-17 14:13:53 +0000

    Update init_data

--- init_db.pl
+++ init_db.pl
@@ -5,35 +5,67 @@ use JSON        qw(from_json);
 use DBI;
 
 my $dbh = DBI->connect( "dbi:SQLite:dbname=database.db", "", "" );
+
+# Plugins schema
 $dbh->do(q{DROP TABLE IF EXISTS plugins});
 $dbh->do(
     q{
     CREATE TABLE plugins (
-        id INTEGER PRIMARY KEY,   -- PK
-        name TEXT UNIQUE,         -- Plugin name
-        description TEXT,         -- Plugin description
-        author TEXT,              -- Author of the plugin
-        thumbnail TEXT,           -- Thumbnail image
+        id INTEGER PRIMARY KEY AUTOINCREMENT,   -- PK
+        name TEXT UNIQUE,                       -- Plugin name
+        description TEXT,                       -- Plugin description
+        author TEXT,                            -- Author of the plugin
+        thumbnail TEXT,                         -- Thumbnail image
         timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
     );
 }
 );
 
+# Versions schema
 $dbh->do(q{DROP TABLE IF EXISTS versions});
 $dbh->do(
     q{
     CREATE TABLE versions (
-        plugin_id INTEGER,        -- id from the plugins table
-        plugin_version TEXT,      -- version of the plugin
-        koha_version TEXT,        -- major Koha version (e.g. '24.05')
-        date_released DATETIME,   -- Date time this plugin version was released
+        plugin_id INTEGER PRIMARY KEY AUTOINCREMENT, --id from the plugins table
+        plugin_version TEXT,                         -- version of the plugin
+        koha_version TEXT,                           -- major Koha version (e.g. '24.05')
+        date_released DATETIME,                      -- Date time this plugin version was released
         FOREIGN KEY(plugin_id) REFERENCES plugins(id) ON DELETE CASCADE
     );
 }
 );
 
-$dbh->do( q{INSERT INTO plugins ( name, description, author, thumbnail ) values ( 'Acquisitions 2.0', 'Replaces the built-in Acquisitions module with a newer, more user-friendly, version.', 'PTFS-Europe', 'acq2.png') });
-$dbh->do( q{INSERT INTO plugins ( name, description, author, thumbnail ) values ( 'Coverflow', 'Provides the option of showing a coverflow carousel in the OPAC through a pre-configured report.', 'Bywater Solutions', 'coverflow.png' ) });
-$dbh->do( q{INSERT INTO plugins ( name, description, author, thumbnail ) values ( 'Event Management', 'This plugin makes it easy for you to create, manage and advertise events to your target audiences.', 'LMS Cloud', NULL ) });
+# Users schema
+$dbh->do(q{DROP TABLE IF EXISTS users});
+$dbh->do(
+    q{
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        email TEXT NOT NULL
+    );
+}
+);
 
-$dbh->do( q{INSERT INTO versions( plugin_id, plugin_version, koha_version ) values( 1, 'v3.1.1', '24.05' ) });
+# Plugins data
+$dbh->do(
+q{INSERT INTO plugins ( name, description, author, thumbnail ) values ( 'Acquisitions 2.0', 'Replaces the built-in Acquisitions module with a newer, more user-friendly, version.', 'PTFS-Europe', 'acq2.png') }
+);
+$dbh->do(
+q{INSERT INTO plugins ( name, description, author, thumbnail ) values ( 'Coverflow', 'Provides the option of showing a coverflow carousel in the OPAC through a pre-configured report.', 'Bywater Solutions', 'coverflow.png' ) }
+);
+$dbh->do(
+q{INSERT INTO plugins ( name, description, author, thumbnail ) values ( 'Event Management', 'This plugin makes it easy for you to create, manage and advertise events to your target audiences.', 'LMS Cloud', NULL ) }
+);
+
+# Versions data
+$dbh->do(
+q{INSERT INTO versions( plugin_id, plugin_version, koha_version ) values( 1, 'v3.1.1', '24.05' ) }
+);
+
+# Users data:
+# admin: admin
+$dbh->do(
+q{INSERT INTO users( username, password, email ) values( 'admin', '$2y$14$suGo48Hu5oujkBqBqzVueeZHjjkNsY1/SZCBtIMFkoDbX.2Vq92yy', 'admin@google.com' ) }
+);
