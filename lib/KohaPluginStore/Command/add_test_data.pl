48e9d8d0a432ba3d41c3e0eb988537388a2b732c
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-07-30 12:37:46 +0000

    Add INSERT OR IGNORE to test data script

--- lib/KohaPluginStore/Command/add_test_data.pl
+++ lib/KohaPluginStore/Command/add_test_data.pl
@@ -5,13 +5,13 @@ my $dbh = DBI->connect( "dbi:SQLite:dbname=database.db", "", "" );
 
 # Plugins data
 $dbh->do(
-q{INSERT INTO plugins ( name, plugin_class, description, author, thumbnail, user_id ) values ( 'Acquisitions 2.0', 'Koha::Plugin::Acquire', 'Replaces the built-in Acquisitions module with a newer, more user-friendly, version.', 'PTFS-Europe', 'acq2.png', 1) }
+q{INSERT OR IGNORE INTO plugins ( name, plugin_class, description, author, thumbnail, user_id ) values ( 'Acquisitions 2.0', 'Koha::Plugin::Acquire', 'Replaces the built-in Acquisitions module with a newer, more user-friendly, version.', 'PTFS-Europe', 'acq2.png', 1) }
 );
 $dbh->do(
-q{INSERT INTO plugins ( name, plugin_class, description, author, thumbnail, user_id ) values ( 'Coverflow', 'Koha::Plugin::Com::ByWaterSolutions::CoverFlow', 'Provides the option of showing a coverflow carousel in the OPAC through a pre-configured report.', 'Bywater Solutions', 'coverflow.png', 2) }
+q{INSERT OR IGNORE INTO plugins ( name, plugin_class, description, author, thumbnail, user_id ) values ( 'Coverflow', 'Koha::Plugin::Com::ByWaterSolutions::CoverFlow', 'Provides the option of showing a coverflow carousel in the OPAC through a pre-configured report.', 'Bywater Solutions', 'coverflow.png', 2) }
 );
 $dbh->do(
-q{INSERT INTO plugins ( name, plugin_class, description, author, thumbnail, user_id ) values ( 'Event Management', 'Koha::Plugin::Com::LMSCloud::EventManagement', 'This plugin makes it easy for you to create, manage and advertise events to your target audiences.', 'LMS Cloud', NULL, 1 ) }
+q{INSERT OR IGNORE INTO plugins ( name, plugin_class, description, author, thumbnail, user_id ) values ( 'Event Management', 'Koha::Plugin::Com::LMSCloud::EventManagement', 'This plugin makes it easy for you to create, manage and advertise events to your target audiences.', 'LMS Cloud', NULL, 1 ) }
 );
 
 # Releases data
@@ -24,9 +24,9 @@ q{INSERT OR IGNORE INTO releases( plugin_id, name, version, koha_max_version ) v
 # John: Doe
 
 $dbh->do(
-q{INSERT INTO users( username, password, email ) values( 'admin', '$2y$14$suGo48Hu5oujkBqBqzVueeZHjjkNsY1/SZCBtIMFkoDbX.2Vq92yy', 'admin@www.com' ) }
+q{INSERT OR IGNORE INTO users( username, password, email ) values( 'admin', '$2y$14$suGo48Hu5oujkBqBqzVueeZHjjkNsY1/SZCBtIMFkoDbX.2Vq92yy', 'admin@www.com' ) }
 );
 
 $dbh->do(
-q{INSERT INTO users( username, password, email ) values( 'John', '$2y$14$0s3kXSp4hBVi5ueaRr1sJez2XGgvV/OETt653a28GHWHCskqV5rRa', 'john@doe.com' ) }
+q{INSERT OR IGNORE INTO users( username, password, email ) values( 'John', '$2y$14$0s3kXSp4hBVi5ueaRr1sJez2XGgvV/OETt653a28GHWHCskqV5rRa', 'john@doe.com' ) }
 );
